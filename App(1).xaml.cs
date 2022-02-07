58d65a9cd044e5bc129bcc7cfc1bd6c2544e5de6
Alrampaz816 <91477969+Alrampaz816@users.noreply.github.com>
2022-02-07 11:19:55 -0500

    Add files via upload
    List of changes:
    - App.xaml.cs: was modified to add an additional constructor to include the local database. 
    -MechAp.Android/MainActivity.cs: was modified to construct a pathway to local database location within the Android file setup
    -MechAp.iOS/AppDelegate.cs: was modified to construct a pathway to local database location within the iOS file setup
    + The User.cs class file was added to define the user in the local database. 
    -Registration.xaml: was modified (1)Eliminated the phone number entry (2)Added the username entry (3)Added password and confirm password entry (4)Password entries are now hidden while user is entering in password (5)keyboard changes when the email entry is being added to make it more simple for the user to enter in their email address. 
    -Registration.xaml.cs was modified within the CreateActBtn_Clicked function, it now will add a new user to the local database. This still needs functionality added to it because it doesn't require all entries and it breaks when a repeat username is entered. I also entered comments in where these fixes should be added.

---
+++ App(1).xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MechAp
+{
+    public partial class App : Application
+    {
+        public static string DatabaseLocation = string.Empty;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new WelcomePage();
+        }
+
+        public App(string databaseLocation) 
+        {
+            InitializeComponent();
+
+            MainPage = new WelcomePage();
+            DatabaseLocation = databaseLocation;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MechAp
+{
+    public class User
+    {
+        [MaxLength(15)]
+        public string FirstName { get; set; }
+
+        [MaxLength(15)]
+        public string LastName { get; set; }
+
+        [PrimaryKey, MaxLength(15)]
+        public string Username { get; set; }
+
+        [MaxLength(14)]
+        public string Password { get; set; }
+
+        [MaxLength(50)]
+        public string Email { get; set; }
+    }
+}
