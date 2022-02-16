673a9e4cdb879c96061d0ea9d158fa2af82a0c98
cianson <92880029+cianson@users.noreply.github.com>
2022-02-15 21:22:05 -0500

    Add files via upload

---
+++ ProcedureView(2).xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MechAp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProcedureView : ContentPage
+    {
+        public ProcedureView()
+        {
+            InitializeComponent();
+        }
+
+        private void MyGarageBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MyGarage();
+        }
+
+        private void MaintMenuBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MaintenanceMenu();
+        }
+
+        private void TrackerViewBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new TrackerView();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            App.Current.MainPage = new MaintenanceMenu();
+            return base.OnBackButtonPressed();
+        }
+    }
+}
---
+++ RegistrationPage(2).xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MechAp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateActBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new LoginPage();
+        }
+
+        private void LoginPgBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new LoginPage();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            App.Current.MainPage = new LoginPage();
+            return base.OnBackButtonPressed();
+        }
+    }
+}
