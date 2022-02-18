bbc17bb09a51ca8d1f1440f8b79bc7b9ea7602bc
Alrampaz816 <91477969+Alrampaz816@users.noreply.github.com>
2022-02-18 11:51:41 -0500

    Add files via upload
    This patch changes:
    -Login Page: so that the password entry is hidden
    -Vehicle Registration Page:
                                      -Keyboard changes for year and milage
                                      -Picker Menus for Make and Model
    -Vehicle Registration Code behind:
                                      -Required Picker menu functions so model picker menu is dynamic based on make picker menu selection
                                      -Adds vehicle to Garage table when add car button is pushed (this uses data binding for the username) and gives success message
    -Added a MainViewModel class so that we can use databinding.

---
+++ MainViewModel(4).cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace MechAp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public string username = string.Empty;
+        public string carName = string.Empty;
+
+        public string Username
+        {
+            get { return Username; }
+            set
+            {
+                username = value;
+
+                OnPropertyChanged(nameof(Username));
+                OnPropertyChanged(nameof(DisplayUsernameInGarage));
+            }
+        }
+
+        public string DisplayUsernameInGarage
+        {
+            get { return "{Username}'s Garage"; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string username)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(username));
+        }
+
+        public string CarName
+        {
+            get => carName;
+            set
+            {
+                if (carName == value)
+                    return;
+                carName = value;
+                OnPropertyChanged(nameof(CarName));
+                OnPropertyChanged(nameof(DisplayCarName));
+            }
+        }
+
+        public string DisplayCarName => $"{CarName}";
+    }
+}
