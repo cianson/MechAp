9d80190db6b21ecdaa42d3d0add8b88eca5895b6
Alrampaz816 <91477969+Alrampaz816@users.noreply.github.com>
2022-02-23 12:56:26 -0500

    Add files via upload

---
+++ User(4).cs
@@ -0,0 +1,44 @@
+﻿using SQLite;
+
+namespace MechAp
+{
+
+    public class User
+    {
+        [MaxLength(15)]
+        public string FirstName { get; set; }
+
+        [MaxLength(15)]
+        public string LastName { get; set; }
+
+        [PrimaryKey, MaxLength(15), NotNull]
+        public string Username { get; set; }
+
+        [MaxLength(14), NotNull]
+        public string Password { get; set; }
+
+        [MaxLength(50), NotNull]
+        public string Email { get; set; }
+    }
+
+    public class Garage
+    {
+
+        public string Username { get; set; }
+
+        [PrimaryKey, NotNull]
+        public string CarName { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string Style { get; set; }
+
+        public string Milage { get; set; }
+
+        public int Year { get; set; }
+
+        public int YearGroup { get; set; }
+    }
+}
