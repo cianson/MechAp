bf8255a63e50bdfe4beaa5e5d7ab6b61c0f38dd1
Alrampaz816 <91477969+Alrampaz816@users.noreply.github.com>
2022-02-28 20:54:45 -0500

    Add files via upload
    Login Page: was modified so that the login credentials are checked against the local database, therefore only reroutes to the Garage if the username and password match "our" records
    MainViewModel is used and I deleted some of the content that we weren't currently using just to make sure that once we need that other code we can annotate it properly. 
    My Garage: was modified so that the list of cars reflects what is shown in the local database. (One flaw is that it currently shows entire Garage table, so all users' vehicles. Currently having a hard time passing the username from page to page since databinding doesn't work that way I found out during this mod, we will need to adjust that once we figure out how to (thumbs up))
    Registration Page: was modified so that it no longer breaks when an new user attempts to input a username that has already been used. Instead it gives a warning and then allows redirects them back to a new registration page.
    Vehicle Registration Page: was modified so that it no longer breaks when a user tries to input a vehicle name that has already been previously used (Again this isn't to check with the association with the username so we will have to modify this query once we figure out how to pass in the username (thumbs up)).

---
+++ MainViewModel(5).cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace MechAp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        string username = string.Empty;
+        public string Username
+        {
+            get => username;
+            set 
+            {
+                if (username == value)
+                    return;
+                username = value;
+                OnPropertyChanged(nameof(Username));
+                OnPropertyChanged(nameof(DisplayUsername));
+            }
+        }
+
+        public string DisplayUsername => $"{Username} 's Garage";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string username)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(username));
+        }
+    }
+}
---
+++ MyGarage(5).xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.Windows.Input;
+
+namespace MechAp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyGarage : ContentPage
+    {
+        public MyGarage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+
+           
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            BindingContext = new MainViewModel();
+
+            string queryString = "SELECT * FROM [Garage]";
+
+            using(SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Garage>();
+                var cars = conn.Query<Garage>(queryString).ToList();
+                GarageInventoryList.ItemsSource = cars;
+            }
+        }
+
+        private void VehicleRegBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new VehicleRegistration();
+        }
+
+        private void TrackerViewBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new TrackerView();
+        }
+
+        private void MaintMenuBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MaintenanceMenu();
+        }
+
+        private void LogOutBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new WelcomePage();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            App.Current.MainPage = new VehicleRegistration();
+            //App.Current.MainPage = new MaintenanceInput();
+            return base.OnBackButtonPressed();
+        }
+    }
+}
