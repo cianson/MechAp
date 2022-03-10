55b4ded14d4772d96f701ed2607fa4dfeeb66767
cianson <92880029+cianson@users.noreply.github.com>
2022-03-10 17:42:54 -0500

    Add files via upload

---
+++ VehicleProfilePage(6).xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MechAp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VehicleProfilePage : ContentPage
+    {
+        public VehicleProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        private void MaintMenuBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MaintenanceMenu();
+        }
+
+        private void TrackerBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new TrackerView();
+        }
+        private void GarageBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MyGarage();
+        }
+    }
+}
