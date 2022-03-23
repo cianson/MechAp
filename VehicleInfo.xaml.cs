25c02c54307ce48889852736d6d89d271e98b058
Alrampaz816 <91477969+Alrampaz816@users.noreply.github.com>
2022-03-23 14:26:17 -0400

    Add files via upload

---
+++ VehicleInfo.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MechAp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VehicleInfo : ContentPage
+    {
+        CurrentUser loggedIn;
+        Garage selectedCar;
+
+        public VehicleInfo()
+        {
+            InitializeComponent();
+        }
+        public VehicleInfo(Garage selectedCar, CurrentUser loggedIn)
+        {
+            InitializeComponent();
+
+            this.selectedCar = selectedCar;
+            this.loggedIn = loggedIn;
+
+            ProfileTitle.Text = selectedCar.CarName.ToString();
+            yearDisplay.Text = selectedCar.Year.ToString();
+            makeDisplay.Text = selectedCar.Make.ToString();
+            modelDisplay.Text = selectedCar.Model.ToString();
+            styleDisplay.Text = selectedCar.Style.ToString();
+        }
+
+        void UpdateBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            App.Current.MainPage = new VehicleProfilePage(selectedCar, loggedIn);
+        }
+
+        void CancelBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            App.Current.MainPage = new VehicleProfilePage(selectedCar, loggedIn);
+        }
+    }
+}
