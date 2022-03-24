9c9876f95d2970ecd190a2aec94e345eda3b9929
Alrampaz816 <91477969+Alrampaz816@users.noreply.github.com>
2022-03-24 12:11:10 -0400

    Add files via upload
    App.xaml has some added styles for the log pages
    TrackerView style has been updated, buttons were added, and the code behind adds functionality to those buttons
    LogItem was added to handle viewing individual completed maintenance items with certain properties being updateable and the whole log is deletable. Its code behind executes that functionality.

---
+++ LogItem(9).xaml.cs
@@ -0,0 +1,165 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MechAp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LogItem : ContentPage
+    {
+        //Variables
+        Garage selectedCar;
+        CurrentUser loggedIn;
+        Tracker selectedLogItem;
+
+        //Constructors
+        public LogItem()
+        {
+            InitializeComponent();
+        }
+
+        public LogItem(Garage selectedCar, CurrentUser loggedIn, Tracker selectedLogItem)
+        {
+            InitializeComponent();
+
+            this.selectedCar = selectedCar;
+            this.loggedIn = loggedIn;
+            this.selectedLogItem = selectedLogItem;
+
+            vehicleTitle.Text = selectedCar.CarName + " Log Item";
+            DateCompletedDisplay.Text = "Date Completed: " + selectedLogItem.DateCompleted;
+            MilageCompletedDisplay.Text = selectedLogItem.AtMilage;
+            MtnTitleDisplay.Text = selectedLogItem.MaintenanceTitle;
+            DueDateDisplay.Text = "Next Service Date: " + selectedLogItem.DateOfNextSErvice;
+            DueMilageDisplay.Text = "Milage for next Service: " + selectedLogItem.MilageOfNextService;
+            reminderSetting.IsToggled = selectedLogItem.ReminderSet;
+        }
+
+        //Navigation Buttons
+        void UpdateBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            //Check Password and then execute update
+            int rows = 0;
+            string username = loggedIn.Username;
+            string passwordEntered = passwordEntry.Text;
+            string queryStatement = "SELECT * FROM [User] WHERE Username = '" + username + "' AND Password = '" + passwordEntered + "'";
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                try
+                {
+                    conn.CreateTable<User>();
+                    rows = conn.Query<User>(queryStatement).Count();
+                    if (rows > 0)
+                    {
+                        int worked = 0;
+
+                        Tracker updateInfo = new Tracker()
+                        {
+                            record_id = selectedLogItem.record_id,
+                            DateCompleted = selectedLogItem.DateCompleted,
+                            Username = loggedIn.Username,
+                            CarName = selectedCar.CarName,
+                            AtMilage = MilageCompletedDisplay.Text,
+                            MaintenanceTitle = MtnTitleDisplay.Text,
+                            ReminderSet = reminderSetting.IsToggled,
+                            DateOfNextSErvice = selectedLogItem.DateOfNextSErvice,
+                            MilageOfNextService = selectedLogItem.MilageOfNextService
+                        };
+
+                        try
+                        {
+                            worked = conn.Update(updateInfo);
+                        }
+                        catch (Exception)
+                        {
+                            DisplayAlert("Oh No", "Your log has NOT been updated", "ok");
+                            App.Current.MainPage = new LogItem(selectedCar, loggedIn, selectedLogItem);
+                        }
+                        if (worked > 0)
+                        {
+                            DisplayAlert("Success", "Your log has been updated", "ok");
+                            App.Current.MainPage = new TrackerView(selectedCar, loggedIn);
+                        }
+                    }
+                    else
+                    {
+                        DisplayAlert("Oops!", "You need to enter your password Correctly to update your log", "Try Again");
+                        App.Current.MainPage = new LogItem(selectedCar, loggedIn, selectedLogItem);
+                    }
+                }
+                catch (Exception)
+                {
+                    DisplayAlert("Sorry", "This maintenance log cannot be updated because the password you entered was incorrect.", "ok");
+                    App.Current.MainPage = new LogItem(selectedCar, loggedIn, selectedLogItem);
+                }
+            }
+        }
+        void DeleteBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            //Display Alert to confirm this is what they want to do -> Check password -> Delete the log item
+            int rows = 0;
+            string username = loggedIn.Username;
+            string passwordEntered = passwordEntry.Text;
+            string queryStatement = "SELECT * FROM [User] WHERE Username = '" + username + "' AND Password = '" + passwordEntered + "'";
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                try
+                {
+                    conn.CreateTable<User>();
+                    rows = conn.Query<User>(queryStatement).Count();
+                    if (rows > 0)
+                    {
+                        int worked = 0;
+
+                        Tracker deleteLog = new Tracker()
+                        {
+                            record_id = selectedLogItem.record_id,
+                            Username = loggedIn.Username,
+                            CarName = selectedCar.CarName,
+                            AtMilage = MilageCompletedDisplay.Text,
+                            MaintenanceTitle = MtnTitleDisplay.Text,
+                            ReminderSet = reminderSetting.IsToggled
+                        };
+
+                        try
+                        {
+                            worked = conn.Delete(deleteLog);
+                        }
+                        catch (Exception)
+                        {
+                            DisplayAlert("Oh No", "Your log has NOT been updated", "ok");
+                            App.Current.MainPage = new LogItem(selectedCar, loggedIn, selectedLogItem);
+                        }
+                        if (worked > 0)
+                        {
+                            DisplayAlert("Success", "Your log has been updated", "ok");
+                            App.Current.MainPage = new TrackerView(selectedCar, loggedIn);
+                        }
+                    }
+                    else
+                    {
+                        DisplayAlert("Oops!", "You need to enter your password Correctly to complete this task.", "Try Again");
+                        App.Current.MainPage = new LogItem(selectedCar, loggedIn, selectedLogItem);
+                    }
+                }
+                catch (Exception)
+                {
+                    DisplayAlert("Sorry", "This maintenance log cannot be updated because the password you entered was incorrect.", "ok");
+                    App.Current.MainPage = new LogItem(selectedCar, loggedIn, selectedLogItem);
+                }
+            }
+        }
+        void BackToLogBtn_Clicked(System.Object sender, System.EventArgs e)
+        {
+            App.Current.MainPage = new TrackerView(selectedCar, loggedIn);
+        }
+    }
+}
---
+++ TrackerView(9).xaml.cs
@@ -0,0 +1,83 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MechAp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TrackerView : ContentPage
+    {
+        //variables
+        Garage selectedCar;
+        CurrentUser loggedIn;
+        string usernameLoggedIn = string.Empty;
+        string carNameSelected = string.Empty;
+
+        //Constructors
+        public TrackerView()
+        {
+            InitializeComponent();
+        }
+        public TrackerView(Garage selectedCar, CurrentUser loggedIn)
+        {
+            InitializeComponent();
+
+            this.selectedCar = selectedCar;
+            this.loggedIn = loggedIn;
+
+            vehicleTitle.Text = selectedCar.CarName + "'s Maintenance Log";
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            usernameLoggedIn = loggedIn.Username;
+            carNameSelected = selectedCar.CarName;
+
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Tracker>();
+                var logItems = conn.Query<Tracker>("SELECT * FROM [Tracker] WHERE Username = ? AND CarName = ?", usernameLoggedIn, carNameSelected).ToList();
+                MaintenanceLog.ItemsSource = logItems;
+            }
+        }
+
+
+        //Navigation Buttons
+        private void MyGarageBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MyGarage(loggedIn);
+        }
+
+        private void MaintInputBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MaintenanceInput(selectedCar, loggedIn);
+        }
+
+        private void MaintMenuBtn_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MaintenanceMenu(selectedCar, loggedIn);
+        }
+
+        private void MaintenanceLog_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedLogItem = MaintenanceLog.SelectedItem as Tracker;
+
+            if (selectedLogItem != null)
+            {
+                App.Current.MainPage = new LogItem(selectedCar, loggedIn, selectedLogItem);
+            }
+        }
+
+        private void ShareBtn_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Sorry", "This function is not yet operational. Check Back soon!", "OK");
+        }
+    }
+}
